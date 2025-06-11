bf3e60177ac9c37cdba634c661e0e3ef63ea8ecf
Zawaad Khan <zawaadkhan6@gmail.com>
2025-06-11 23:38:02 +0600

    Added Borders

--- js/main.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("scroll", () => {
-  const nav = document.querySelector("nav");
-  if (window.scrollY > 10) {
-    nav.classList.add("scrolled");
-  } else {
-    nav.classList.remove("scrolled");
-  }
-});
